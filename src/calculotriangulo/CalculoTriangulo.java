9021dd0fb77b82dad6beb24c0e5c752e82c406b9
Thomas Mauricio Rodriguez <thomasmauricio521@gmail.com>
2023-02-14 07:47:30 -0500

    primera clase

--- src/calculotriangulo/CalculoTriangulo.java
+++ src/calculotriangulo/CalculoTriangulo.java
@@ -6,14 +6,21 @@ import perro.perro;
 public class CalculoTriangulo {
 
     public static void main(String[] args) {
-        
-       perro perro = new perro("ringo", "aleman", 7, true);
-       
-       perro.ladrar();
-       
+
+        //instancias de la clase triangulo y uso de metodos
+        triangulo triangulo = new triangulo();
+
+        System.out.println(triangulo.calcular(2, 5));
+
+        System.out.println(triangulo.tipoTriangulo(5, 5, 5));
+
+        //instancia de la clase perro y uso de metodos y atributos
+        perro perro = new perro("ringo", "aleman", 7, true);
+        perro.ladrar();
+
         System.out.println(perro.getNombre());
         System.out.println(perro.getRaza());
-       
+
     }
 
 }
